40920fb01837ef56eadc53692b3e960ea89d94e5
umerk4466 <umerk4466@gmail.com>
2020-06-04 18:31:21 +0100

    updated

--- screens/all_pages_structure.js
+++ screens/all_pages_structure.js
@@ -7,7 +7,7 @@ export default function all_pages_structure({ navigation }) {
       <ScrollView>
         <View style={styles.main_row}>
           <View style={styles.col}>
-            <Text>Content goes here</Text>
+            <Text>Content goes here Or new view</Text>
           </View>
         </View>
       </ScrollView>
