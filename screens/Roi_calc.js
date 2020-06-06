921034a7ad85ca22405f0a3c6af5a1d1dba25b5d
umerk4466 <umerk4466@gmail.com>
2020-06-06 16:38:27 +0100

    updated

--- screens/Roi_calc.js
+++ screens/Roi_calc.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 // import for tab view
-import { TabView, SceneMap } from "react-native-tab-view";
+import { TabView, TabBar, SceneMap } from "react-native-tab-view";
 
 const FirstRoute = () => (
   <View style={[styles.scene, { backgroundColor: "#ff4081" }]}>
@@ -25,29 +25,49 @@ export default function Roi_calc({ navigation }) {
   const [routes] = React.useState([
     { key: "first", title: "First" },
     { key: "second", title: "Second" },
-    { key: "three", title: "Third" }
+    { key: "three", title: "Third" },
   ]);
 
   const renderScene = SceneMap({
     first: FirstRoute,
     second: SecondRoute,
-    three: Three
+    three: Three,
   });
+
+  // tab bar style customization
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      activeColor="#6d578b"
+      inactiveColor="#c2c3c8"
+      indicatorStyle={{ backgroundColor: '#000' }}
+      style={{ backgroundColor: '#fff' }}
+    />
+  );
 
   return (
     <TabView
+      renderTabBar={renderTabBar}
       navigationState={{ index, routes }}
       renderScene={renderScene}
       onIndexChange={setIndex}
       initialLayout={initialLayout}
+      // tabBarPosition={}
+      swipeEnabled={true}
+    // tabBarPosition={"bottom"}
+    // sceneContainerStyle={{ paddingVertical: 10 }}
+    // style={{ margin: 10, borderBottomColor: "red" }}
+    // swipeVelocityImpact={29000}
+    // onSwipeEnd={(dun) => alert("jsj")}
+    // timingConfig={{ duration: 100000 }}
     />
   );
 }
 
 const styles = StyleSheet.create({
   scene: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
 // import { StyleSheet, Text, View } from "react-native";
