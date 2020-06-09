93e972e04ff48d24d897b4713f02cd359b97ff16
umerk4466 <umerk4466@gmail.com>
2020-06-09 15:50:46 +0100

    updated

--- screens/Basic_roi_calc.js
+++ screens/Basic_roi_calc.js
@@ -3,38 +3,37 @@ import { SafeAreaView, ScrollView, Text, View, TextInput, Button } from "react-n
 // input field for money
 import { TextInputMask } from "react-native-masked-text";
 // import global styles
-import { globalstyles } from '../styles/global_styles'
+import { globalstyles } from "../styles/global_styles";
 // import form and form validator(formik, yup) library
-import * as yup from 'yup'
-import { Formik } from 'formik'
-
+import * as yup from "yup";
+import { Formik } from "formik";
 
 const ReviewBasicForm = yup.object({
-  // monthly_rental: yup.number().required().positive().integer(),
-  // monthly_mortgage: yup.number().positive().integer(),
-  // initial_deposit: yup.number().required().positive().integer(),
-  // monthly_rental: yup.string().required().test('is_valid', "Please enter the valid number", (val) => {
-  //   return val === "£1"
-  // }),
-
-
-})
-
+  monthly_rental: yup.number("Must be a number").required("Monthly rental must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
+  monthly_mortgage: yup.number("Must be a number").required("Mortgage must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
+  initial_deposit: yup.number("Must be a number").required("Deposit must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
+});
 
 export default function Basic_roi_calc() {
   return (
     <Formik
-      initialValues={{ monthly_rental: '', monthly_mortgage: '', initial_deposit: '', }}
+      initialValues={{ monthly_rental: "", monthly_mortgage: "", initial_deposit: "" }}
       validationSchema={ReviewBasicForm}
       onSubmit={(values, actions) => {
-        actions.resetForm()
-        console.log(values)
+        // actions.resetForm();
+        // console.log(typeof values.monthly_mortgage);
+        console.log(values);
       }}
     >
-      {(props) => (
+      {props => (
         <SafeAreaView style={globalstyles.container}>
           <ScrollView>
             <View style={globalstyles.main_row}>
+              <View style={globalstyles.col}>
+                <View style={globalstyles.result_container}>
+                  <Text> Result </Text>
+                </View>
+              </View>
               <View style={globalstyles.col}>
                 <Text>Monthly rental</Text>
                 <TextInputMask
@@ -51,12 +50,15 @@ export default function Basic_roi_calc() {
                   placeholder={"£500"}
                   keyboardType={"decimal-pad"}
                   value={props.values.monthly_rental}
+                  onBlur={props.handleBlur("monthly_rental")}
                   includeRawValueInChangeText={true}
                   onChangeText={(maskedText, rawText) => {
                     // props.handleChange("monthly_rental")
-                    props.setFieldValue('monthly_rental', rawText)
+                    props.setFieldValue("monthly_rental", rawText);
                   }}
                 />
+                {props.errors.monthly_rental && props.touched.monthly_rental ? <Text style={globalstyles.error_field}>{props.errors.monthly_rental}</Text> : null}
+
 
                 <Text>Mortgage interest pcm</Text>
                 <TextInputMask
@@ -73,11 +75,14 @@ export default function Basic_roi_calc() {
                   placeholder={"£200"}
                   keyboardType={"decimal-pad"}
                   value={props.values.monthly_mortgage}
+                  onBlur={props.handleBlur("monthly_mortgage")}
                   includeRawValueInChangeText={true}
                   onChangeText={(maskedText, rawText) => {
-                    props.setFieldValue('monthly_mortgage', rawText)
+                    props.setFieldValue("monthly_mortgage", rawText);
                   }}
                 />
+                {props.errors.monthly_mortgage && props.touched.monthly_mortgage ? <Text style={globalstyles.error_field}>{props.errors.monthly_mortgage}</Text> : null}
+
 
                 <Text>Initial investment (deposit)</Text>
                 <TextInputMask
@@ -94,19 +99,20 @@ export default function Basic_roi_calc() {
                   placeholder={"£500"}
                   keyboardType={"decimal-pad"}
                   value={props.values.initial_deposit}
+                  onBlur={props.handleBlur("initial_deposit")}
                   includeRawValueInChangeText={true}
                   onChangeText={(maskedText, rawText) => {
-                    props.setFieldValue('initial_deposit', rawText)
+                    props.setFieldValue("initial_deposit", rawText);
                   }}
                 />
+                {props.errors.initial_deposit && props.touched.initial_deposit ? <Text style={globalstyles.error_field}>{props.errors.initial_deposit}</Text> : null}
 
                 <Button title="Submit" onPress={props.handleSubmit} />
               </View>
             </View>
           </ScrollView>
         </SafeAreaView>
-      )
-      }
+      )}
     </Formik>
-  )
+  );
 }
--- styles/global_styles.js
+++ styles/global_styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from "react-native";
+import { shadow } from "react-native-paper";
 
 // styles used in all the pages
 export const globalstyles = StyleSheet.create({
@@ -14,11 +15,31 @@ export const globalstyles = StyleSheet.create({
     },
     input: {
         marginVertical: 5,
-        height: 35,
+        height: 32,
         // maxWidth: "100%",
         borderWidth: 0.5,
-        borderRadius: 3,
-        fontSize: 16
+        borderRadius: 2,
+        fontSize: 16,
+    },
+    error_field: {
+        color: "crimson",
+        fontSize: 12,
+        marginBottom: 5,
+    },
+    result_container: {
+        backgroundColor: "white",
+        height: 100,
+        justifyContent: 'center',
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        elevation: 2,
+
 
     }
 });
