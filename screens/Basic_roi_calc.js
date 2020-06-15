1ad6c28e5045029a0a08a3a6ebd956314a237dae
unknown <umerk4466@gmail.com>
2020-06-15 15:36:49 +0100

    updated

--- screens/Basic_roi_calc.js
+++ screens/Basic_roi_calc.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { SafeAreaView, ScrollView, Text, View, TextInput, } from "react-native";
-import { Button, Tooltip } from "react-native-elements";
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import { Button } from "react-native-elements";
 // input field for money
 import { TextInputMask } from "react-native-masked-text";
 // import global styles
@@ -11,24 +10,42 @@ import * as yup from "yup";
 import { Formik } from "formik";
 
 const ReviewBasicForm = yup.object({
-  monthly_rental: yup.number("Must be a number").required("Monthly rental must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
-  monthly_mortgage: yup.number("Must be a number").required("Mortgage must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
-  initial_deposit: yup.number("Must be a number").required("Deposit must be at least of £0").integer("Must be integer").typeError("Must enter a number again"),
+  monthly_rental: yup
+    .number("Must be a number")
+    .required("Monthly rental must be at least of £0")
+    .integer("Must be integer")
+    .typeError("Must enter a number again"),
+  monthly_mortgage: yup
+    .number("Must be a number")
+    .required("Mortgage must be at least of £0")
+    .integer("Must be integer")
+    .typeError("Must enter a number again"),
+  initial_deposit: yup
+    .number("Must be a number")
+    .required("Deposit must be at least of £0")
+    .integer("Must be integer")
+    .typeError("Must enter a number again")
   // final_result: yup.number()
 });
 
 export default function Basic_roi_calc() {
   return (
     <Formik
-      initialValues={{ monthly_rental: "", monthly_mortgage: "", initial_deposit: "", final_result: 0 }}
+      initialValues={{
+        monthly_rental: "",
+        monthly_mortgage: "",
+        initial_deposit: "",
+        final_result: 0
+      }}
       validationSchema={ReviewBasicForm}
       enableReinitialize={true}
       onSubmit={(values, actions) => {
         // get all the filed data and add Basic return on investment formula
-        let annual_cash_flow = (values.monthly_rental * 12) - (values.monthly_mortgage * 12);
-        let annual_roi = (annual_cash_flow / values.initial_deposit) * 100
+        let annual_cash_flow =
+          values.monthly_rental * 12 - values.monthly_mortgage * 12;
+        let annual_roi = (annual_cash_flow / values.initial_deposit) * 100;
         // update the "final_result field"
-        actions.setFieldValue("final_result", annual_roi.toFixed(0))
+        actions.setFieldValue("final_result", annual_roi.toFixed(0));
       }}
     >
       {props => (
@@ -38,13 +55,17 @@ export default function Basic_roi_calc() {
               <View style={globalstyles.col}>
                 <View style={globalstyles.result_container}>
                   <Text style={{ fontSize: 16 }}> Return on investment </Text>
-                  <Text style={{ fontSize: 17 }}> {props.values.final_result}%</Text>
+                  <Text style={{ fontSize: 17 }}>
+                    {" "}
+                    {props.values.final_result}%
+                  </Text>
                 </View>
               </View>
               <View style={globalstyles.col}>
-                <Tooltip height={110} popover={<Text style={{ color: "white" }}>Monthly rent of the property which will come in your pocket after all the expenses.</Text>}>
+                {/* <Tooltip height={110} popover={<Text style={{ color: "white" }}>Monthly rent of the property which will come in your pocket after all the expenses.</Text>}>
                   <Text>Monthly rent of the property <FontAwesome name={'question-circle'} size={15}></FontAwesome></Text>
-                </Tooltip>
+                </Tooltip> */}
+                <Text>Monthly rent of the property</Text>
                 <TextInputMask
                   type={"money"}
                   options={{
@@ -66,12 +87,13 @@ export default function Basic_roi_calc() {
                     props.setFieldValue("monthly_rental", rawText);
                   }}
                 />
-                {props.errors.monthly_rental && props.touched.monthly_rental ? <Text style={globalstyles.error_field} numberOfLines={1}>{props.errors.monthly_rental}</Text> : null}
+                {props.errors.monthly_rental && props.touched.monthly_rental ? (
+                  <Text style={globalstyles.error_field} numberOfLines={1}>
+                    {props.errors.monthly_rental}
+                  </Text>
+                ) : null}
 
-
-                <Tooltip height={130} popover={<Text style={{ color: "white" }}>Mortgage of the property to pay each month or enter other monthly expenses.</Text>}>
-                  <Text>Monthly Mortgage payments <FontAwesome name={'question-circle'} size={15}></FontAwesome></Text>
-                </Tooltip>
+                <Text>Monthly Mortgage payments</Text>
                 <TextInputMask
                   type={"money"}
                   options={{
@@ -92,11 +114,14 @@ export default function Basic_roi_calc() {
                     props.setFieldValue("monthly_mortgage", rawText);
                   }}
                 />
-                {props.errors.monthly_mortgage && props.touched.monthly_mortgage ? <Text style={globalstyles.error_field} numberOfLines={1}>{props.errors.monthly_mortgage}</Text> : null}
+                {props.errors.monthly_mortgage &&
+                props.touched.monthly_mortgage ? (
+                  <Text style={globalstyles.error_field} numberOfLines={1}>
+                    {props.errors.monthly_mortgage}
+                  </Text>
+                ) : null}
 
-                <Tooltip height={100} popover={<Text style={{ color: "white" }}>Your saved money which you will use as a deposit to buy this property.</Text>}>
-                  <Text>Initial investment (deposit) <FontAwesome name={'question-circle'} size={15}></FontAwesome></Text>
-                </Tooltip>
+                <Text>Initial investment (deposit)</Text>
                 <TextInputMask
                   type={"money"}
                   options={{
@@ -117,16 +142,27 @@ export default function Basic_roi_calc() {
                     props.setFieldValue("initial_deposit", rawText);
                   }}
                 />
-                {props.errors.initial_deposit && props.touched.initial_deposit ? <Text style={globalstyles.error_field} numberOfLines={1}>{props.errors.initial_deposit}</Text> : null}
+                {props.errors.initial_deposit &&
+                props.touched.initial_deposit ? (
+                  <Text style={globalstyles.error_field} numberOfLines={1}>
+                    {props.errors.initial_deposit}
+                  </Text>
+                ) : null}
 
                 <View style={globalstyles.col}>
                   <View style={globalstyles.buttons_container}>
-                    <Button title="Calculate ROI" onPress={props.handleSubmit} />
+                    <Button
+                      title="Calculate ROI"
+                      onPress={props.handleSubmit}
+                    />
                     <Button title="Reset" onPress={props.resetForm} />
                   </View>
-                  <Text style={globalstyles.calculator_explanation_text}>ROI (Return on Investment) measures the gain or loss generated on an investment relative to the amount of money invested.</Text>
+                  <Text style={globalstyles.calculator_explanation_text}>
+                    ROI (Return on Investment) measures the gain or loss
+                    generated on an investment relative to the amount of money
+                    invested.
+                  </Text>
                 </View>
-
               </View>
             </View>
           </ScrollView>
